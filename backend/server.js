fcbdf6466e9d5e40f83300d36f00c692aaa65ad9
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-10 20:45:14 +0200

    add userModel

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-app.use("/api/users", require("./routes/usersRoute"));
+app.use("/api/users", require("./routes/userRoute"));
 
 app.use(errorHandler);
 
