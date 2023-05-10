96bbab045df73d9f63fab3031d4a0c3839ecab2d
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-10 21:38:51 +0200

    add registration of a new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 // @desc   Register new user
 // @route  /api/users
@@ -10,14 +12,43 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("One of the fields is empty");
     }
-})
+
+    // Check if a user already exists
+    const userEmail = await User.findOne({ email });
+    if (userEmail) {
+        res.status(400);
+        throw new Error("User with this email already exists");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = User.create({
+        name,
+        email,
+        password: hashPassword
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data");
+    }
+});
 
 // @desc   Login user
 // @route  /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
     res.send("Login route");
-})
+});
 
 module.exports = {
     registerUser,
