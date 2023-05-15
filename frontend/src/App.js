cd49e0b0ada98873a18e9872155c612ec73a8ca6
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-16 00:05:54 +0200

    add Home, Login, Register pages, add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
