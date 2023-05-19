9e63f3f136cf513a6d11c9a7bcac6a668b155ab5
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-19 17:57:17 +0200

    add login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,64 @@
+import {useState} from "react";
+import {toast} from "react-toastify";
+import {Link} from "react-router-dom";
+
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChangeInput = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        console.log(formData);
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <>
+            <h1 className="page-title">Log in to your account</h1>
+            <form className="form" name="login" onSubmit={onSubmit}>
+                <div className="form__field">
+                    <label htmlFor="email" className="sr-only">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="form__input"
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__field">
+                    <label htmlFor="password" className="sr-only">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="form__input"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__submit">
+                    <button type="submit" className="form__submit-btn btn btn--accent">Login</button>
+                </div>
+                <footer className="form__footer">Don't have an account? <Link to="/register">Signup</Link></footer>
+            </form>
+        </>
     );
 };
 
