f255abbcaa7e32ab606b22ba5fbc924c4eb3200b
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-19 17:52:35 +0200

    add register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,19 +9,22 @@ import Footer from "./components/Footer";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <main className="main">
-                <div className="main__container container">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                    </Routes>
-                </div>
-            </main>
-            <Footer />
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Header />
+                <main className="main">
+                    <div className="main__container container">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                        </Routes>
+                    </div>
+                </main>
+                <Footer />
+            </BrowserRouter>
+            <ToastContainer />
+        </>
     );
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,97 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const { name, email, password, confirmPassword } = formData;
+
+    const onChangeInput = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            toast.error("Passwords are not equal");
+            return;
+        }
+
+        console.log(formData);
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+            <h1 className="page-title">Create your account</h1>
+            <form className="form" name="register" onSubmit={onSubmit}>
+                <div className="form__field">
+                    <label htmlFor="name" className="sr-only">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="form__input"
+                        name="name"
+                        value={name}
+                        placeholder="Name"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__field">
+                    <label htmlFor="email" className="sr-only">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="form__input"
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__field">
+                    <label htmlFor="password" className="sr-only">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="form__input"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__field">
+                    <label htmlFor="confirmPassword" className="sr-only">Confirm password</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        className="form__input"
+                        name="confirmPassword"
+                        value={confirmPassword}
+                        placeholder="Confirm password"
+                        onChange={onChangeInput}
+                        required
+                    />
+                </div>
+                <div className="form__submit">
+                    <button type="submit" className="form__submit-btn btn btn--accent">Create account</button>
+                </div>
+                <footer className="form__footer">Do you have an account? <Link to="/login">Login</Link></footer>
+            </form>
+        </>
     );
 };
 
