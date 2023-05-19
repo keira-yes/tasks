43be9058b7783cc1c037ba2bb66133b7e5d3c138
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-19 18:11:48 +0200

    add home page content

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import { IoCreateOutline } from "react-icons/io5";
+import { HiOutlineViewGrid } from "react-icons/hi";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <h1 className="home__title page-title">A better online to-do list app for work</h1>
+            <p className="home__text">App makes it easier for a team to plan their work by using online to-do lists.</p>
+            <div className="home__buttons">
+                <Link to="/create-task" className="btn btn--transparent"><IoCreateOutline /> Create a new task</Link>
+                <Link to="/tasks" className="btn btn--accent"><HiOutlineViewGrid /> My tasks</Link>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,5 @@
-import {useState} from "react";
-import {toast} from "react-toastify";
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const [formData, setFormData] = useState({
