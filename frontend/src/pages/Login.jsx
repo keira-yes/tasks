bc6aa117a2d7349603ee37a2c83ce801abaf62a7
Irina Kirillova <keira.kirillova@gmail.com>
2023-05-19 18:14:29 +0200

    update markup of register and login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,41 +23,43 @@ const Login = () => {
     }
 
     return (
-        <>
-            <h1 className="page-title">Log in to your account</h1>
-            <form className="form" name="login" onSubmit={onSubmit}>
-                <div className="form__field">
-                    <label htmlFor="email" className="sr-only">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        className="form__input"
-                        name="email"
-                        value={email}
-                        placeholder="Email"
-                        onChange={onChangeInput}
-                        required
-                    />
-                </div>
-                <div className="form__field">
-                    <label htmlFor="password" className="sr-only">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="form__input"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        onChange={onChangeInput}
-                        required
-                    />
-                </div>
-                <div className="form__submit">
-                    <button type="submit" className="form__submit-btn btn btn--accent">Login</button>
-                </div>
-                <footer className="form__footer">Don't have an account? <Link to="/register">Signup</Link></footer>
-            </form>
-        </>
+        <div className="login">
+            <h1 className="login__title page-title">Log in to your account</h1>
+            <div className="login__form">
+                <form className="form" name="login" onSubmit={onSubmit}>
+                    <div className="form__field">
+                        <label htmlFor="email" className="sr-only">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="form__input"
+                            name="email"
+                            value={email}
+                            placeholder="Email"
+                            onChange={onChangeInput}
+                            required
+                        />
+                    </div>
+                    <div className="form__field">
+                        <label htmlFor="password" className="sr-only">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="form__input"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={onChangeInput}
+                            required
+                        />
+                    </div>
+                    <div className="form__submit">
+                        <button type="submit" className="form__submit-btn btn btn--accent">Login</button>
+                    </div>
+                    <footer className="form__footer">Don't have an account? <Link to="/register">Signup</Link></footer>
+                </form>
+            </div>
+        </div>
     );
 };
 
