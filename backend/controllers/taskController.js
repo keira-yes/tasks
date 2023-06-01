b8514c9fe13d8e6c79c362697f67827bb0cbd601
Irina Kirillova <keira.kirillova@gmail.com>
2023-06-01 12:34:40 +0200

    add updateTask and deleteTask on server

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -71,8 +71,66 @@ const getTask = asyncHandler(async (req, res) => {
     res.status(200).json(task);
 });
 
+// @desc   Update task
+// @route  PUT /api/tasks/:id
+// @access Private
+const updateTask = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+        res.status(404);
+        throw new Error("Task not found");
+    }
+
+    if (task.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    res.status(200).json(updatedTask);
+});
+
+// @desc   Delete task
+// @route  DELETE /api/tasks/:id
+// @access Private
+const deleteTask = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+        res.status(404);
+        throw new Error("Task not found");
+    }
+
+    if (task.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+
+    await task.deleteOne();
+
+    res.status(200).json({ success: true, message: "Task has been deleted" });
+});
+
 module.exports = {
     getTasks,
     createTask,
-    getTask
+    getTask,
+    updateTask,
+    deleteTask
 }
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { authToken } = require("../middleware/authToken");
-const { getTasks, createTask, getTask } = require("../controllers/taskController");
+const { getTasks, createTask, getTask, updateTask, deleteTask } = require("../controllers/taskController");
 
 router.route("/")
     .get(authToken, getTasks)
     .post(authToken, createTask);
 
-router.get("/:id", authToken, getTask);
+router.route("/:id")
+    .get(authToken, getTask)
+    .put(authToken, updateTask)
+    .delete(authToken, deleteTask);
 
 module.exports = router;
