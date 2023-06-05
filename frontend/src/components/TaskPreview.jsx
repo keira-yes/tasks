0e9ee575adce34f90cc8267eb0fac8179c658950
Irina Kirillova <keira.kirillova@gmail.com>
2023-06-05 23:09:40 +0200

    add styles to task preview

--- frontend/src/components/TaskPreview.jsx
+++ frontend/src/components/TaskPreview.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
+import { IoPricetagsOutline } from "react-icons/io5";
 
 const TaskPreview = ({ task }) => {
-    const { createdAt, description, status, _id } = task;
+    const { status, createdAt, description, category, _id } = task;
 
     return (
         <div className="card">
             <div className="card__content">
+                <span className={`card__status card__status--${status}`}>{status}</span>
                 <span className="card__date">{new Date(createdAt).toLocaleString()}</span>
                 <p className="card__description">{description}</p>
             </div>
             <div className="card__info">
-                <span className={`card__status card__status--${status}`}>{status}</span>
-                <Link to={`/tasks/${_id}`}>View</Link>
+                <span className={`card__category card__category--${category}`}><IoPricetagsOutline /> {category}</span>
+                <Link to={`/tasks/${_id}`} className="btn btn--accent btn--sm">View</Link>
             </div>
         </div>
     );
