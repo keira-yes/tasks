4b790eb74272cc18a546f67dfe3961dc60a4334e
Irina Kirillova <keira.kirillova@gmail.com>
2023-06-12 23:04:48 +0200

    change response in frontend on development and production modes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -13,13 +14,19 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.send("Tasks API");
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/tasks", require("./routes/taskRoute"));
+
+// Frontend in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"));
+} else {
+    app.get("/", (req, res) => {
+        res.send("Tasks API");
+    });
+}
 
 app.use(errorHandler);
 
